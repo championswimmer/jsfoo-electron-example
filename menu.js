34622bf12d37ea46f1c25c34a39bd6359f8e845c
Arnav Gupta <arnav@codingblocks.com>
2016-09-14 11:52:19 +0530

    add custom menu item in main
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- menu.js
+++ menu.js
@@ -1,188 +1,26 @@
-const {Menu} = require('electron')
+const { Menu, app, shell } = require('electron');
+const defaultMenu = require('electron-default-menu');
+const dialog = require('dialog');
 
-const template = [
-    {
-        label: 'MyMenu',
-        submenu: [
-            {
-                label: 'Custom Action'
-            }
-        ]
-    },
-    {
-        label: 'Edit',
-        submenu: [
-            {
-                role: 'undo'
-            },
-            {
-                role: 'redo'
-            },
-            {
-                type: 'separator'
-            },
-            {
-                role: 'cut'
-            },
-            {
-                role: 'copy'
-            },
-            {
-                role: 'paste'
-            },
-            {
-                role: 'pasteandmatchstyle'
-            },
-            {
-                role: 'delete'
-            },
-            {
-                role: 'selectall'
-            }
-        ]
-    },
-    {
-        label: 'View',
-        submenu: [
-            {
-                label: 'Reload',
-                accelerator: 'CmdOrCtrl+R',
-                click (item, focusedWindow) {
-                    if (focusedWindow) focusedWindow.reload()
-                }
-            },
-            {
-                label: 'Toggle Developer Tools',
-                accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-                click (item, focusedWindow) {
-                    if (focusedWindow) focusedWindow.webContents.toggleDevTools()
-                }
-            },
-            {
-                type: 'separator'
-            },
-            {
-                role: 'resetzoom'
-            },
-            {
-                role: 'zoomin'
-            },
-            {
-                role: 'zoomout'
-            },
-            {
-                type: 'separator'
-            },
-            {
-                role: 'togglefullscreen'
-            }
-        ]
-    },
-    {
-        role: 'window',
-        submenu: [
-            {
-                role: 'minimize'
-            },
-            {
-                role: 'close'
-            },
-            {
-                label: 'Custom Action'
-            }
-        ]
-    },
-    {
-        role: 'help',
-        submenu: [
-            {
-                label: 'Learn More',
-                click () { require('electron').shell.openExternal('http://electron.atom.io') }
-            },
-            {
-                label: 'Custom Action'
-            }
-        ]
-    }
-]
+app.on('ready', () => {
 
-if (process.platform === 'darwin') {
-    const name = require('electron').remote.app.getName()
-    template.unshift({
-        label: name,
+    // Get template for default menu
+    const menu = defaultMenu(app, shell);
+
+    // Add custom menu
+    menu.splice(4, 0, {
+        label: 'Custom',
         submenu: [
             {
-                role: 'about'
-            },
-            {
-                type: 'separator'
-            },
-            {
-                role: 'services',
-                submenu: []
-            },
-            {
-                type: 'separator'
-            },
-            {
-                role: 'hide'
-            },
-            {
-                role: 'hideothers'
-            },
-            {
-                role: 'unhide'
-            },
-            {
-                type: 'separator'
-            },
-            {
-                role: 'quit'
+                label: 'Do something',
+                click: (item, focusedWindow) => {
+                    dialog.info("Yay did something ! ", "Done something")
+                }
             }
         ]
     })
-    // Edit menu.
-    template[1].submenu.push(
-        {
-            type: 'separator'
-        },
-        {
-            label: 'Speech',
-            submenu: [
-                {
-                    role: 'startspeaking'
-                },
-                {
-                    role: 'stopspeaking'
-                }
-            ]
-        }
-    )
-    // Window menu.
-    template[3].submenu = [
-        {
-            label: 'Close',
-            accelerator: 'CmdOrCtrl+W',
-            role: 'close'
-        },
-        {
-            label: 'Minimize',
-            accelerator: 'CmdOrCtrl+M',
-            role: 'minimize'
-        },
-        {
-            label: 'Zoom',
-            role: 'zoom'
-        },
-        {
-            type: 'separator'
-        },
-        {
-            label: 'Bring All to Front',
-            role: 'front'
-        }
-    ]
-}
 
-const menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
+    // Set top-level application menu, using modified template
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
+})
+ 
